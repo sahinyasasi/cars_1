0cc96f5035f901ca5fd7ff2a8c41e74902c3a5f0
sahinyasasi <sahinya.slc@gmail.com>
2020-12-31 14:12:59 +0530

    displaying added cars

--- public/main.js
+++ public/main.js
@@ -3,13 +3,14 @@ const update2 = document.querySelector("#update-button2");
 const update3 = document.querySelector("#update-button3");
 const submitBtn = document.querySelector("#submit");
 const carsList = document.querySelector(".cars");
+const addedCar = document.querySelector(".addedCar");
 
 update1.addEventListener("click", () => {
-  carsList.classList.remove("hidden");
+  addedCar.classList.remove("hidden");
 });
 update2.addEventListener("click", () => {
-  carsList.classList.remove("hidden");
+  addedCar.classList.remove("hidden");
 });
 update3.addEventListener("click", () => {
-  carsList.classList.remove("hidden");
+  addedCar.classList.remove("hidden");
 });
