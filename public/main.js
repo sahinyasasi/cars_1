97501a290eb86460ea4e3e36d2324f24af4fd2a1
sahinyasasi <sahinya.slc@gmail.com>
2020-12-31 13:50:56 +0530

    hidden list

--- public/main.js
+++ public/main.js
@@ -1,2 +1,15 @@
-const update = document.querySelector("#update-button");
-const submitBtn = document.querySelector("#submitBtn");
+const update1 = document.querySelector("#update-button1");
+const update2 = document.querySelector("#update-button2");
+const update3 = document.querySelector("#update-button3");
+const submitBtn = document.querySelector("#submit");
+const carsList = document.querySelector(".cars");
+
+update1.addEventListener("click", () => {
+  carsList.classList.remove("hidden");
+});
+update2.addEventListener("click", () => {
+  carsList.classList.remove("hidden");
+});
+update3.addEventListener("click", () => {
+  carsList.classList.remove("hidden");
+});
